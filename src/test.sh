9cc471791862d5e2cfc4ef4d13fcec1165f92395
fjktkm <fjktkm@gmail.com>
2024-04-02 18:39:47 +0000

    fix: 不要なオプションを除去

--- src/test.sh
+++ src/test.sh
@@ -18,20 +18,20 @@ git add .gitattributes
 git commit -m "Setup custom merge driver for zip files"
 
 # ベースファイルの作成
-echo -e "Hello, World!\nLine 2: Base version\nLine 3: Common line" > testfile.txt
+echo "Hello, World!\nLine 2: Base version\nLine 3: Common line" > testfile.txt
 zip content.zip testfile.txt
 git add content.zip
 git commit -m "Add base zip file content.zip"
 
 # ブランチの作成と変更の適用
 git checkout -b branch_local
-echo -e "Hello, World!\nLine 2: Local version\nLine 3: Common line\nLine 4: Added by Local" > testfile.txt
+echo "Hello, World!\nLine 2: Local version\nLine 3: Common line\nLine 4: Added by Local" > testfile.txt
 zip content.zip testfile.txt
 git add content.zip
 git commit -m "Local changes in content.zip"
 git checkout main
 
-echo -e "Hello, World!\nLine 2: Remote version\nLine 3: Common line\nLine 5: Added by Remote" > testfile.txt
+echo "Hello, World!\nLine 2: Remote version\nLine 3: Common line\nLine 5: Added by Remote" > testfile.txt
 zip content.zip testfile.txt
 git add content.zip
 git commit -m "Remote changes in content.zip"
